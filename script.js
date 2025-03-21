8b8dd7ef9eafd1be3ae0178b14439f5f478ffc2c
georgejcmp <george.moreno@yahoo.com.br>
2025-03-20 23:45:58 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+let circulo = document.querySelector(".circulo")
+let imagem = document.querySelector(".imagem-copo")
+
+function trocarcor(cor){
+    circulo.style.background = cor
+}
+
+function trocaimagem(img){
+    imagem.src = img
+}
